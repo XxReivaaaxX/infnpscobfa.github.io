43dd19154ef126453369bbe1ae8efdc8b83a3b04
XxReivaaaxX <deryan765@gmail.com>
2024-12-11 17:08:58 -0500

    c

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './inicio.css';
+import App from './inicio.html';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
